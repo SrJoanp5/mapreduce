42bf4031e801dbb8f8eee955865aee4b7b903419
SrJoanp5 <62654835+SrJoanp5@users.noreply.github.com>
2022-02-09 19:24:19 +0100

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,25 @@
+import sys
+import io
+import re
+import nltk
+import string
+nltk.download('stopwords',quiet=True)
+from nltk.corpus import stopwords
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+stop_words = set(stopwords.words('english'))
+input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='latin1')
+for line in input_stream:
+  line = line.strip()
+  line = re.sub(r'[^\w\s]', '',line)
+  line = line.lower()
+  for x in line:
+    if x in punctuations:
+      line=line.replace(x, " ") 
+
+  words=line.split()
+  for word in words: 
+    if word not in stop_words:
+        letra = word[0:1]
+        if letra in list(string.ascii_lowercase):
+          print('%s\t%s' % (letra, 1))
